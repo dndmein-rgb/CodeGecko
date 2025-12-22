1052844dbea90494e86315a58c21de80c9345287
Divyanshu <dnd.mein@gmail.com>
2025-12-22 15:21:54 +0530

    Implemented better-auth authentication system

---
+++ lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from "./db";
+// If your Prisma file is located elsewhere, you can change the path
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    }),
+    socialProviders:{
+        github:{
+            clientId:process.env.GITHUB_CLIENT_ID!,
+            clientSecret:process.env.GITHUB_CLIENT_SECRET,
+            scope:["repo"]
+        }
+    }
+});

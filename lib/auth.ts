c62ba5be30f6650e31fdd5b80087ac6b9b6dcd9d
Divyanshu <dnd.mein@gmail.com>
2025-12-29 10:40:46 +0530

    Integrated Polar for subscription management

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,103 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
-// If your Prisma file is located elsewhere, you can change the path
+import {
+  polar,
+  checkout,
+  portal,
+  usage,
+  webhooks,
+} from "@polar-sh/better-auth";
+import { polarClient } from "@/module/payment/config/polar";
+import { updatePolarCustomerId, updateUserTier } from "@/module/payment/lib/subscription";
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: ["repo"],
+    },
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "3314baa1-44c2-44ac-9ccb-5725b6bbfa80",
+              slug: "codehorse", // Custom slug for easy reference in Checkout URL, e.g. /checkout/codehorse
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal({
+          returnUrl:
+            process.env.NEXT_PUBLIC_APP_URL ||
+            "http://localhost:3000/dashboard",
+        }),
+        usage(),
+        webhooks({
+          secret: process.env.POLAR_WEBHOOK_SECRET!,
+          onSubscriptionActive: async (payload) => {
+            const customerId = payload.data.customerId;
+            const user = await prisma.user.findUnique({
+              where: {
+                polarCustomerId: customerId,
+              },
+            });
+            if (user) {
+              await updateUserTier(user.id, "PRO", "ACTIVE", payload.data.id);
+            }
+          },
+          onSubscriptionCanceled: async (payload) => {
+            const customerId = payload.data.customerId;
+            const user = await prisma.user.findUnique({
+              where: {
+                polarCustomerId: customerId,
+              },
+            });
+            if (user) {
+              await updateUserTier(
+                user.id,
+                user.subscriptionTier as any,
+                "CANCELED"
+              );
+            }
+          },
+          onSubscriptionCreated: async (payload) => {
+             const customerId = payload.data.customerId;
+            const user = await prisma.user.findUnique({
+              where: {
+                polarCustomerId: customerId,
+              },
+            });
+            if (user) {
+              await updateUserTier(
+                user.id,
+                user.subscriptionTier as any,
+                "EXPIRED"
+              );
+            }
+          },
+          onCustomerCreated: async (payload) => {
+            const user=await prisma.user.findUnique({
+                where:{
+                    email:payload.data.email
+                }
+            })
+            if(user){
+                await updatePolarCustomerId(user.id,payload.data.id)
+            }
+          },
+        }),
+      ],
     }),
-    socialProviders:{
-        github:{
-            clientId:process.env.GITHUB_CLIENT_ID!,
-            clientSecret:process.env.GITHUB_CLIENT_SECRET,
-            scope:["repo"]
-        }
-    }
+  ],
 });
--- module/payment/lib/subscription.ts
+++ module/payment/lib/subscription.ts
@@ -198,7 +198,7 @@ export async function updatePolarCustomerId(
     await prisma.user.update({
         where:{id:userId},
         data:{
-            // polarCustomerId
+            polarCustomerId
         }
     })
 }
